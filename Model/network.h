6cc601f02cfa32a4f5fd874ca37192dd00d71e2d
PeleAB <35038104+PeleAB@users.noreply.github.com>
2025-06-20 22:09:27 +0300

    added network
    added network

--- Model/network.h
+++ Model/network.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network.h
   * @author  STEdgeAI
-  * @date    2025-06-17 21:45:07
+  * @date    2025-06-20 20:02:31
   * @brief   Minimal description of the generated c-implemention of the network
   ******************************************************************************
   * @attention
@@ -20,7 +20,7 @@
 
 /******************************************************************************/
 #define LL_ATON_DEFAULT_C_MODEL_NAME        "Default"
-#define LL_ATON_DEFAULT_ORIGIN_MODEL_NAME   "quantized_tiny_yolo_v2_224_"
+#define LL_ATON_DEFAULT_ORIGIN_MODEL_NAME   "MediaPipeFaceLandmarkDetector"
 
 /************************** USER ALLOCATED IOs ********************************/
 // No user allocated inputs
@@ -28,14 +28,17 @@
 
 /************************** INPUTS ********************************************/
 #define LL_ATON_DEFAULT_IN_NUM        (1)    // Total number of input buffers
-// Input buffer 1 -- Input_0_out_0
+// Input buffer 1 -- Input_1_out_0
 #define LL_ATON_DEFAULT_IN_1_ALIGNMENT   (32)
-#define LL_ATON_DEFAULT_IN_1_SIZE_BYTES  (150528)
+#define LL_ATON_DEFAULT_IN_1_SIZE_BYTES  (110592)
 
 /************************** OUTPUTS *******************************************/
-#define LL_ATON_DEFAULT_OUT_NUM        (1)    // Total number of output buffers
-// Output buffer 1 -- Transpose_55_out_0
+#define LL_ATON_DEFAULT_OUT_NUM        (2)    // Total number of output buffers
+// Output buffer 1 -- Quantize_242_out_0
 #define LL_ATON_DEFAULT_OUT_1_ALIGNMENT   (32)
-#define LL_ATON_DEFAULT_OUT_1_SIZE_BYTES  (5880)
+#define LL_ATON_DEFAULT_OUT_1_SIZE_BYTES  (1)
+// Output buffer 2 -- Transpose_302_out_0
+#define LL_ATON_DEFAULT_OUT_2_ALIGNMENT   (32)
+#define LL_ATON_DEFAULT_OUT_2_SIZE_BYTES  (1404)
 
 #endif /* LL_ATON_DEFAULT_H */
