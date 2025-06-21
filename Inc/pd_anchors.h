6955e42fd01bcc8e667f81353dc85b6318a94915
PeleAB <35038104+PeleAB@users.noreply.github.com>
2025-06-21 17:00:12 +0300

    Fix anchor struct redefinition

--- Inc/pd_anchors.h
+++ Inc/pd_anchors.h
@@ -1,15 +1,8 @@
 #ifndef PD_ANCHORS_H
 #define PD_ANCHORS_H
 
-#include "pd_pp_output_if.h"
+#include "pd_model_pp_if.h"
 #include "blazeface_anchors.h"
-
-typedef struct {
-    float32_t x;
-    float32_t y;
-    float32_t w;
-    float32_t h;
-} pd_anchor_t;
 
 extern pd_anchor_t g_Anchors[BLAZEFACE_NUM_ANCHORS];
 void pd_anchor_init(void);
