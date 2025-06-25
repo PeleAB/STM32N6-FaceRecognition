b8afc1e75b5e69671554e8d95d7f5175fafa3e79
PeleAB <35038104+PeleAB@users.noreply.github.com>
2025-06-25 17:00:12 +0300

    working recognition flow
    working recognition flow

--- python_tools/run_face_recognition.py
+++ python_tools/run_face_recognition.py
@@ -61,7 +61,7 @@ def inflate_box(box: np.ndarray, factor: float = 1.2) -> np.ndarray:
 
 def main() -> None:
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--image", help="Input image path", default="trump.jpg")
+    parser.add_argument("--image", help="Input image path", default="trump3.jpg")
     parser.add_argument(
         "--rec-model",
         default="models/mobilefacenet_fp32_PerChannel_quant_lfw_test_data_npz_1_OE_3_2_0.onnx",
@@ -76,6 +76,7 @@ def main() -> None:
     parser.add_argument(
         "--visualize",
         action="store_true",
+        default=True,
         help="Show the detected face and the aligned crop",
     )
     args = parser.parse_args()
